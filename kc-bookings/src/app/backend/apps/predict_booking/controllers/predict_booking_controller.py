609a0f7f8137e97129ac1959b0fd1e1deeb3a606
Shrijan Choudhary <shrijan.choudhary@az-rgm-02.fractal.com>
2024-06-08 11:25:31 +0000

    added api for file upload

--- kc-bookings/src/app/backend/apps/predict_booking/controllers/predict_booking_controller.py
+++ kc-bookings/src/app/backend/apps/predict_booking/controllers/predict_booking_controller.py
@@ -4,8 +4,11 @@ Controllers for Filter Panel APIs
 
 from apps.core.utils import BaseResponseHandler
 from apps.predict_booking.dtos import PredictBookingDto
-from flask import request
+from apps.predict_booking.services import BookingPredictions
+from flask import request,jsonify 
 from flask_restx import Resource
+import pandas as pd
+import io
 
 api = PredictBookingDto.api
 responseHandler = BaseResponseHandler()
@@ -33,3 +36,43 @@ class FirstBookingController(Resource):
         except Exception as e:
             return responseHandler.error_response(
                 "Booking not found", 401, str(e), 401)
+
+
+@api.route("/upload")
+class UploadTestController(Resource):
+    """Controller for getting uploaded csv"""
+
+    @api.response(
+        PredictBookingDto.service_available_response_code,
+        PredictBookingDto.service_available_response_message,
+        PredictBookingDto.booking_response_obj,
+    )
+    @api.response(
+        PredictBookingDto.service_unavailable_response_code,
+        PredictBookingDto.service_unavailable_response_message,
+        PredictBookingDto.booking_response_obj,
+    )
+    def post(self):
+        """Returns the data uploaded"""
+        if 'file' not in request.files:
+            return responseHandler.error_response(
+                "File not found", 401,
+                'No file part in the request', 401)
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+        if file:
+            try:
+                # Read the file into a pandas DataFrame
+                df = pd.read_csv(io.StringIO(file.stream.read().decode('UTF-8')))
+                # get the predictions
+                obj_pred_bookings = BookingPredictions()
+                df = obj_pred_bookings.main(df)
+                # Convert DataFrame to JSON for demonstration purposes
+                df = df.head(100)
+                data = df.to_dict(orient='records')
+                # print(data)
+                # return jsonify({'data': data}), 200
+                return responseHandler.success_response(200, None, data)
+            except Exception as e:
+                return jsonify({'error': str(e)}), 500
